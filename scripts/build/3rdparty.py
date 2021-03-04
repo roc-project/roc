0ba15a95c47f5476809a3dc83a9da14834cb7c55
Victor Gaydov <victor@enise.org>
2021-03-04 13:21:06 +0300

    Fix building old pulseaudio on recent distros

--- scripts/build/3rdparty.py
+++ scripts/build/3rdparty.py
@@ -805,6 +805,7 @@ elif name == 'pulseaudio':
             '--disable-manpages',
             '--disable-orc',
             '--disable-webrtc-aec',
+            '--disable-openssl',
             '--without-caps',
         ])), logfile)
     execute_make(logfile)
